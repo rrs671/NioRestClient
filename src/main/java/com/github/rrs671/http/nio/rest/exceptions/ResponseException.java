aa9d1d9326a6a10797706ad960b694bebffd1490
Rafael Rodrigues de Souza <rafael.rodrigues.de.souza35@gmail.com>
2025-03-01 20:53:32 -0300

    Request response treatment treatment with useful methods and code refactory.

---
+++ src/main/java/com/github/rrs671/http/nio/rest/exceptions/ResponseException.java
@@ -0,0 +1,24 @@
+package com.github.rrs671.http.nio.rest.exceptions;
+
+public class ResponseException extends RuntimeException {
+
+    public ResponseException() {
+        super();
+    }
+
+    public ResponseException(String message) {
+        super(message);
+    }
+
+    public ResponseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResponseException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ResponseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
